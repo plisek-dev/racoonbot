5dcc6c92254fbaa78c05517b2db6202d2e6ba2a9
Paweł Lisek <135659343+plisek-dev@users.noreply.github.com>
2025-01-21 16:16:41 +0100

    Update racoonbot_simple.launch.py

--- src/racoonbot_pkg/bringup/launch/racoonbot_simple.launch.py
+++ src/racoonbot_pkg/bringup/launch/racoonbot_simple.launch.py
@@ -24,7 +24,7 @@ from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
     # Declare arguments
-    # declared_arguments = []
+    declared_arguments = []
     # declared_arguments.append(
     #     DeclareLaunchArgument(
     #         "gui",
