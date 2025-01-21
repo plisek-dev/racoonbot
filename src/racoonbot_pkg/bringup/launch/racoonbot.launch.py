ebaf2846a822ecf153305624b26ed9d34440e642
Paweł Lisek <135659343+plisek-dev@users.noreply.github.com>
2025-01-21 15:49:54 +0100

    Update racoonbot.launch.py

--- src/racoonbot_pkg/bringup/launch/racoonbot.launch.py
+++ src/racoonbot_pkg/bringup/launch/racoonbot.launch.py
@@ -66,9 +66,9 @@ def generate_launch_description():
             "racoonbot_controllers.yaml",
         ]
     )
-    rviz_config_file = PathJoinSubstitution(
-        [FindPackageShare("racoonbot_pkg"), "rviz", "racoonbot.rviz"]
-    )
+    #rviz_config_file = PathJoinSubstitution(
+    #    [FindPackageShare("racoonbot_pkg"), "rviz", "racoonbot.rviz"]
+    #)
 
     control_node = Node(
         package="controller_manager",
@@ -88,14 +88,14 @@ def generate_launch_description():
             ("/diff_drive_controller/cmd_vel_unstamped", "/cmd_vel"),
         ],
     )
-    rviz_node = Node(
-        package="rviz2",
-        executable="rviz2",
-        name="rviz2",
-        output="log",
-        arguments=["-d", rviz_config_file],
-        condition=IfCondition(gui),
-    )
+    #rviz_node = Node(
+    #    package="rviz2",
+    #    executable="rviz2",
+    #    name="rviz2",
+    #    output="log",
+    #    arguments=["-d", rviz_config_file],
+    #    condition=IfCondition(gui),
+    #)
 
     joint_state_broadcaster_spawner = Node(
         package="controller_manager",
@@ -110,12 +110,12 @@ def generate_launch_description():
     )
 
     # Delay rviz start after `joint_state_broadcaster`
-    delay_rviz_after_joint_state_broadcaster_spawner = RegisterEventHandler(
-        event_handler=OnProcessExit(
-            target_action=joint_state_broadcaster_spawner,
-            on_exit=[rviz_node],
-        )
-    )
+    #delay_rviz_after_joint_state_broadcaster_spawner = RegisterEventHandler(
+    #    event_handler=OnProcessExit(
+    #        target_action=joint_state_broadcaster_spawner,
+    #        on_exit=[rviz_node],
+    #    )
+    #)
 
     # Delay start of robot_controller after `joint_state_broadcaster`
     delay_robot_controller_spawner_after_joint_state_broadcaster_spawner = RegisterEventHandler(
@@ -129,7 +129,7 @@ def generate_launch_description():
         control_node,
         robot_state_pub_node,
         joint_state_broadcaster_spawner,
-        delay_rviz_after_joint_state_broadcaster_spawner,
+    #    delay_rviz_after_joint_state_broadcaster_spawner,
         delay_robot_controller_spawner_after_joint_state_broadcaster_spawner,
     ]
 
