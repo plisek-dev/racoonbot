a098d0ea797e5711c3ad2f7f56453765f3dfa853
Paweł Lisek <135659343+plisek-dev@users.noreply.github.com>
2025-01-22 22:21:52 +0100

    Delete build directory

--- build/ldlidar/colcon_command_prefix_build.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/pawel/Workspaces/racoonbot/install/ldlidar_component/share/ldlidar_component/package.sh"
-. "/home/pawel/Workspaces/racoonbot/install/ldlidar_node/share/ldlidar_node/package.sh"
--- build/racoonbot_pkg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pawel/Workspaces/racoonbot/src/racoonbot_pkg
-# Build directory: /home/pawel/Workspaces/racoonbot/build/racoonbot_pkg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
